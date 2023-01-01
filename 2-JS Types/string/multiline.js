965f3bf54cacc49cb26c7f0c2b0c091c3bc28ffa
Mahmud <jmbengal.mbstu@gmail.com>
2023-01-01 17:56:33 +0600

    string added

---
+++ 2-JS Types/string/multiline.js
@@ -0,0 +1,13 @@
+// using the + operator
+const message1 =
+  "This is a long message " +
+  "that spans across multiple lines" +
+  "in the code.";
+
+// using the \ operator
+const message2 =
+  "This is a long message \
+that spans across multiple lines \
+in the code.";
+console.log(message1)
+console.log(message2)
---
+++ 2-JS Types/string/string obj.js
@@ -0,0 +1,6 @@
+const a = 'hello';
+const b = new String('hello');
+console.log(a); // "hello"
+console.log(b); // "hello"
+console.log(typeof a); // "string"
+console.log(typeof b); // "object"
