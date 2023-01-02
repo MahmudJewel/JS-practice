633dfbea2a5e9da02ecf4fcd853116fda5a5d4d4
Mahmud <jmbengal.mbstu@gmail.com>
2023-01-02 11:49:26 +0600

    setInterval added

---
+++ async/2-setInterval.js
@@ -0,0 +1,5 @@
+// program to display a text using setInterval method
+function greet() {
+  console.log("Hello world");
+}
+setInterval(greet, 1000);
---
+++ async/displaytime using setInterval.js
@@ -0,0 +1,10 @@
+// program to display time every 5 seconds
+function showTime() {
+    // return new date and time
+    let dateTime= new Date();
+    // return the time
+    let time = dateTime.toLocaleTimeString();
+    console.log(time)
+}
+let display = setInterval(showTime, 5000);
+
