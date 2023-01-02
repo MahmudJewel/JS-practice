cb1d43882666c817ca151402ddf337502ab02b1d
Mahmud <jmbengal.mbstu@gmail.com>
2023-01-02 17:45:37 +0600

    async function added

--- async/async.js
+++ async/async.js
@@ -2,6 +2,4 @@ async function name(parameter1, parameter2, ...paramaterN) {
   // statements
 }
 
-asyncFunc().catch(
-  // catch error and do something
-  );
+
--- demo.js
+++ demo.js
@@ -1,12 +1,14 @@
-// constructor function
-function Person () {
-  this.name = 'Mahmud',
-  this.age = 23
-}
-// creating objects
-let person1 = new Person();
-let person2 = new Person();
-// adding new property to constructor function
-Person.prototype.gender = 'Male';
-console.log(person1.gender); // Male
-console.log(person2.gender); // Male
+// // constructor function
+// function Person () {
+//   this.name = 'Mahmud',
+//   this.age = 23
+// }
+// // creating objects
+// let person1 = new Person();
+// let person2 = new Person();
+// // adding new property to constructor function
+// Person.prototype.gender = 'Male';
+// console.log(person1.gender); // Male
+// console.log(person2.gender); // Male
+
+console.info([]==[])
