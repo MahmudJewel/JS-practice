10f3b884dde0b7c22e81ce944801ce518884e3f0
Mahmud <jmbengal.mbstu@gmail.com>
2023-01-04 13:18:44 +0600

    strict mode added

---
+++ use strict.js
@@ -0,0 +1,22 @@
+
+// // 01
+// //"use strict";
+// a = 10; // ok
+// console.log(a);
+// function ok() {
+//   "use strict";
+//   x = 50; // error, let x = 50
+//   return x;
+// }
+// console.log(ok());
+
+// // //02
+// "use strict"
+// let a = 010 // error
+// console.log(a)
+
+// // 03
+// "use strict"
+// person = {name: 'Carla', age: 25}; // throws an error
+// console.log(person)
+
