c62375564726f20c3fda376c5439184deae3b4e2
Mahmud <jmbengal.mbstu@gmail.com>
2023-01-05 10:13:32 +0600

    obj campare added

---
+++ obj/obj compare.js
@@ -0,0 +1,15 @@
+// modified: https://gist.github.com/nicbell/6081098
+// Object.compare = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+const comp = (a, b) => {
+    if(JSON.stringify(a) === JSON.stringify(b)){
+        return true
+    }
+    else{
+        return false
+    }
+};
+
+a = { k: 1 };
+b = { k: 1 };
+console.log(comp(a,b))
+
